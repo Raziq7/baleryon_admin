c773609ecb89b7c9f92483d581fcf784abef1327
Raz3-33 <raziq@ocelots.in>
2025-09-20 12:48:08 +0530

    url changed

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,9 +1,10 @@
 // api.js (or axios.js)
-import axios from 'axios';
+import axios from "axios";
 
 // Create an Axios instance with a base URL
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: "https://admin.baleryon.in/api",
 });
 
 export default api;
